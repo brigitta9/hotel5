6a8e559c78b34e32a88519cc64fec040fb3ee3bd
brigitta9 <75415492+brigitta9@users.noreply.github.com>
2020-12-16 11:16:47 +0700

    Add files via upload

---
+++ data/titikpusat_1.js
@@ -0,0 +1,8 @@
+var json_titikpusat_1 = {
+"type": "FeatureCollection",
+"name": "titikpusat_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.822488541298711, -6.171630447042195 ] } }
+]
+}
